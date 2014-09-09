ebac5dad7851b6e41dda6e025b8ac5170bc48be7
David Best <davidbest@Davids-MacBook-Pro-2.local>
2014-09-09 11:26:39 -0400

    Modifying the way services are loaded by Sails
    Sails .10 changed the way services are loaded. To make our directory
    structure work, they need to be reloaded with a new set of options.
    This reload is facilitated by the bootstrap script.
    
    See #217

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -7,10 +7,18 @@
  * For more information on bootstrapping your app, check out:
  * http://sailsjs.org/#documentation
  */
+var buildDictionary = require('sails-build-dictionary');
 
 module.exports.bootstrap = function (cb) {
-
-  // It's very important to trigger this callack method when you are finished 
+  buildDictionary.optional({
+        dirname     : sails.config.paths.services,
+        filter      : /(.+)\.(js|coffee|litcoffee)$/,
+        depth     : 2,
+        caseSensitive : true
+      }, function (err, modules) {
+           sails.services = modules;
+           cb();
+         });
+  // It's very important to trigger this callback method when you are finished 
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  cb();
 };
