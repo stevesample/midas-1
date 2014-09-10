a9e5d05d225b635167a9bb520246a56c0d0d282b
David Best <davidbest@Davids-MacBook-Pro-2.local>
2014-09-10 09:47:51 -0400

    Changing the model migration strategy.
    Previously, the migration strategy was set to 'drop'. With that setting,
    the tags created in make init would be dropped when sails lift was run.
    Setting to alter corrects that behavior.

--- config/models.js
+++ config/models.js
@@ -16,6 +16,6 @@ module.exports.models = {
   * See http://sailsjs.org/#/documentation/concepts/ORM/model-settings.html  *
   *                                                                          *
   ***************************************************************************/
-  migrate: 'drop'
+  migrate: 'alter'
 
 };
