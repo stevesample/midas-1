7b319043db205f176d23ea59c6c769d2fb0e42fd
Rio Flores <rio517@gmail.com>
2015-11-09 17:02:11 +0100

    Config: uncomment setting port from env variables

--- config/application.js
+++ config/application.js
@@ -21,7 +21,7 @@ module.exports = {
   //
   // In production, you'll probably want to change this setting
   // to 80 (http://) or 443 (https://) if you have an SSL certificate
-  // port: process.env.PORT || 1337,
+  port: process.env.PORT || 1337,
 
   // The runtime "environment" of your Sails app is either 'development' or 'production'.
   //
